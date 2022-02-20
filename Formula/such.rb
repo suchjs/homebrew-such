6670ad8ce0f35765d84acf3c4550adc568b524af
fefit <jxz_211@163.com>
2022-02-20 08:05:05 +0800

    fix: add scripts to pkg

--- Formula/such.rb
+++ Formula/such.rb
@@ -2,7 +2,7 @@ class Such < Formula
   desc "Generate fake data, customizable and powerful."
   homepage "https://www.suchjs.com"
   url "https://github.com/suchjs/such-cli/releases/download/v0.2.1/such-0.2.1.tar.gz"
-  sha256 "3de53db4ce50eeae9c2a963761fb8c4518d9386dcb9d54774d375ee9c587ee99"
+  sha256 "116325194ef58204318dcf92960150274ed91073a7a7ba358f96da9117ec7ceb"
   license "MIT"
   version "0.2.1"
 
